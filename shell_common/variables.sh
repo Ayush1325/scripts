0f9c58910f80719215ad1aab45ab17577ff7ace3
Ayush Singh <ayushdevel1325@gmail.com>
2024-03-04 21:23:36 +0530

    Remove stuff moved to dotfiles
    Signed-off-by: Ayush Singh <ayushdevel1325@gmail.com>

--- shell_common/variables.sh
+++
@@ -1,8 +0,0 @@
-export EDITOR=/usr/bin/nvim
-export VISUAL=/usr/bin/nvim
-export DENO_INSTALL="/var/home/ayush/.deno"
-
-path+=("$HOME/.local/bin")
-path+=("$HOME/flutter/bin")
-path+=("$HOME/.local/share/nvim/lsp_servers/rust_analyzer")
-path+=("$DENO_INSTALL/bin")
