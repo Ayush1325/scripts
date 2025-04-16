d82dd63b7bd80a5ff94cc210a5d51c86b1957179
Ayush Singh <ayush@beagleboard.org>
2025-04-16 13:14:59 +0530

    rclone: Allow extra args
    - Initial runs need bisync
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- rclone/personal.sh
+++ rclone/personal.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rclone bisync PersonalDrive:Personal\ Docs/ ~/Documents/PersonalDocs -P -v
+rclone bisync PersonalDrive:Personal\ Docs/ ~/Documents/PersonalDocs -P -v $@
--- rclone/university.sh
+++ rclone/university.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rclone bisync PersonalDrive:University\ Material/ ~/Documents/University -P -v
+rclone bisync PersonalDrive:University\ Material/ ~/Documents/University -P -v $@
